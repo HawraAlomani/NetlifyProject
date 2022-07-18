1b5deffeff367e55de0454203f4b1f3a157e99b0
Hawra Alomani <ha0073@mix.wvu.edu>
2022-07-18 13:31:41 +0300

    ready for deployment

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Good Afternoon, I'm Hawra</h1>
+      <h5> First Deployment Test</h5>
+    </div>
+  );
+}
+
+export default App;
